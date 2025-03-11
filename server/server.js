84b0fa02510c774996a67017b784472a43a4df52
Ayush Katare <katareayush2005@gmail.com>
2025-03-11 13:01:37 +0530

    allowed all origin

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://dataset-saas-rose.vercel.app",
+    origin: "*" ,
     credentials: true,
   })
 );
