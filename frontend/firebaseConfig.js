a56ff58a0b9f66e759872612ed68dd80d207edb5
CarlGustav <singhrajvardhan2003@gmail.com>
2025-03-11 11:22:44 +0530

    deployment

--- frontend/firebaseConfig.js
+++ frontend/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDBVy-OvEtqBf5yGbwWy_KLHS4j_A0qJR4",
-    authDomain: "riverlytics.firebaseapp.com",
-    projectId: "riverlytics",
-    storageBucket: "riverlytics.firebasestorage.app",
-    messagingSenderId: "907247668690",
-    appId: "1:907247668690:web:45157692e03c52aa190c0b",
-    measurementId: "G-WGNLJ2HEC9"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
   };
 
 // Initialize Firebase
